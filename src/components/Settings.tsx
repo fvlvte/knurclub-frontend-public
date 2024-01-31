58a5b645647be336af76dd2ed89d43f3cf0155b9
fvlvte <me@falat.me>
2024-01-31 13:03:22 -0700

    Fix building.

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,5 @@
-import { default as axios } from "axios";
-import { Config } from "../Config";
-import { useEffect } from "react";
-import { V2SR } from "./V2SR";
-
 export const Settings: React.FC = () => {
-  const u = new URLSearchParams(window.location.search);
+  //const u = new URLSearchParams(window.location.search);
 
   return (
     <>
