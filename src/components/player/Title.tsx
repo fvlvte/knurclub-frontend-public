c3ae8aa6178cbf5eaa869fe1b6fba48e7b228985
Michał Itrych <129063907+M-Itrych@users.noreply.github.com>
2024-03-05 22:14:06 +0100

    Width Scaling added (#6)

--- src/components/player/Title.tsx
+++ src/components/player/Title.tsx
@@ -9,7 +9,7 @@ const Title = ({ title }: TitleProps) => {
         alignSelf: 'stretch',
         color: 'var(--base-white, #FFF)',
         fontFamily: 'Inter',
-        fontSize: '4rem',
+        fontSize: 'calc(4rem + (100vw - 800px) / 100)',
         fontStyle: 'normal',
         fontWeight: '700',
         lineHeight: 'normal',
